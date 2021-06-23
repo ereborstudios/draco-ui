fce2d420657f922d639dd7c6c0b8a7a81d8a0214
Logan Koester <logan@logankoester.com>
2021-06-23 18:50:57 +0700

    Fixes a bug that was causing mouse clicks to be triggered on buttons belonging to a previous scene

--- templates/app/systems/mouse_focus.rb
+++ templates/app/systems/mouse_focus.rb
@@ -1,10 +1,12 @@
 class MouseFocus < Draco::System
-  filter ButtonLabel
+  filter ButtonLabel, Position
 
   def tick(args)
     return if entities.nil?
 
-    entities.each do |entity|
+    current = world.scene || world
+
+    current.filter([ButtonLabel, Position]).each do |entity|
       if args.inputs.mouse.point.inside_rect?(entity_rect(entity))
         unless entity.components[:focused]
           entity.components << Focused.new
